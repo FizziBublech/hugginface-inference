aad9b29100f61348c343b01601e7616b5a048535
FizziBublech <107878420+FizziBublech@users.noreply.github.com>
2023-10-01 20:23:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post('/text2image', async (req, res) => {
 })
 
 // Start the server
-const port = 3800
+const port = process.env.PORT || 3800;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
 })
